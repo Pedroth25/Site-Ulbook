2efa1996d03d0ce6d1eb420fc6d8d549fad5f663
Pedroth25 <145405637+Pedroth25@users.noreply.github.com>
2023-09-19 00:56:41 +0100

    Add files via upload
    Fazendo o upload dos arquivos do site Ulbook

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Ulbook</title>
+</head>
+<body>
+<div class="cir"></div>
+    <header>
+    
+        <div class="logo">
+                <a href="#"> <img src="assets/img/open-book (1).png" alt="logo"></a><p>Ulbook</p>
+        </div>
+        <nav class="navigatiion">
+            <ul >
+                <li><a href="#">Home</a></li>
+                <li> <a href="#">Sobre</a></li>
+                <li><a href="#">Servicos</a></li>
+                <li><a href="#">Contactos</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="home" id="home">
+        <div class="text">
+        <h2>ULbook</h2>
+        <h3>O seu material de apoio online totalmente <span>Gratuito!</span></h3>
+        <P>Matricule-se agora na Ulbook e poderá usufruir de um maerial de apoio <br>Totalmente gratuito sendo a primeira plataforma Angolana do genero </P>
+        <a href="#">Começar Agora</a>
+        </div>
+        <ul class="social">
+            <li><a href="#"> <img src="assets/img/facebook (1).png" alt="facebook"></a></li>
+            <li>  <a href="#"> <img src="assets/img/whatsapp (1).png" alt="whatsapp"></a></li>
+            <li> <a href="#"> <img src="assets/img/instagram (1).png" alt="instagram"></a></li>
+        </ul>
+        <div class="fot"></div>
+      
+    </section>
+
+    
+</body>
+</html>
